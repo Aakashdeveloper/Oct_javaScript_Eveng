2afffe041c2e15ca0cf5076906bac7f05de2abbe
Aakashdeveloper <ahanda205@gmail.com>
2020-10-16 20:37:37 +0530

    A

---
+++ 2datatype.js
@@ -0,0 +1,46 @@
+true is equal to 1
+false is equal to 0
+
+var a = true
+var b = true 
+a+b
+2
+true+false
+1
+true+10
+11
+10-true
+9
+10+false
+10
+
+true+'true'
+"truetrue"
+'true'+2
+"true2"
+
+///////////////
+var a = '1'
+var b = '2'
+a+b
+'12'
+
+//es5
+parseInt(a)+parseInt(b)
+3
+var a = '1.3534'
+var b = '2'
+undefined
+parseInt(a)+parseInt(b)
+3
+
+var a = '1.3534'
+var b = '2'
+undefined
+parseInt(a)+parseInt(b)
+3
+parseFloat(a)+parseFloat(b)
+3.3533999999999997
+
+//es6
+Number(a)+Number(b)
---
+++ 3promptconfirm.js
@@ -0,0 +1,32 @@
+console.log() >> for cheak output and debugger;
+alert()> For Notification
+confirm() > to check weather yes or no;
+prompt() > to take input for user;
+
+alert('Form Submitted')
+
+confirm('Do you want to leave the page')
+true
+confirm('Do you want to leave the page')
+false
+var out = confirm('Do you want to leave the page')
+undefined
+out
+true
+var out = confirm('Do you want to leave the page')
+undefined
+out
+false
+
+prompt('Tell me your name')
+"Aakash"
+var out = prompt('What is your age')
+undefined
+out
+"10"
+
+//////////////
+var a = prompt('Please enter first number');
+var b = prompt('Please enter second number');
+var output = Number(a)+Number(b)
+alert("Output value is"+output);
