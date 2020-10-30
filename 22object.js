24eabfbec9234637799b833c936f2be2f97ee5a1
Aakashdeveloper <ahanda205@gmail.com>
2020-10-30 20:42:35 +0530

    a

--- 22object.js
+++ 22object.js
@@ -33,3 +33,26 @@ movies
 {name: "Death Race", rating: 3.5, ind: "Hollywood"}
 
 movies['name']
+
+
+/////////
+var calc = {
+    sum:function(a,b) {return a+b},
+    sub:(a,b) => {return a-b}
+}
+undefined
+calc.sum(3,6)
+9
+calc.sub(9,6)
+3
+
+var dbQuery = {
+    find:(table)=>{return `Select * from ${table}`},
+    insert:(table,data) => {return `Insert into ${table} name,city values (${data.name},${data.city})`}
+}
+
+dbQuery.find('Emp')
+"Select * from Emp"
+
+dbQuery.insert('Student',{name:'John',city:'Delhi'})
+"Insert into Student name,city values (John,Delhi)"
