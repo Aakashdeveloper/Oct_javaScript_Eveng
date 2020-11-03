71fb6bcd0f89cbe465c65a321e23953bc138fdfc
Aakashdeveloper <ahanda205@gmail.com>
2020-11-03 20:49:45 +0530

    a

---
+++ 30Calc/calc.js
@@ -0,0 +1,23 @@
+const calc = (opt) => {
+    var a = document.getElementById('first').value;
+    var b = document.getElementById('second').value;
+    var out;
+    if(opt == "add"){
+        out = Number(a)+Number(b)
+    }else{
+        out = Number(a)-Number(b)
+    }
+    document.getElementById('output').innerText=out
+}
+/*function add(){
+    var a = document.getElementById('first').value;
+    var b = document.getElementById('second').value;
+    var out = Number(a)+Number(b)
+    document.getElementById('output').innerText=out
+}
+const sub = () => {
+    var a = document.getElementById('first').value;
+    var b = document.getElementById('second').value;
+    var out = Number(a)-Number(b)
+    document.getElementById('output').innerText=out
+}*/
