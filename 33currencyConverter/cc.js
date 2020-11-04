cf499ad2ea83a97fd0ccc71ea7e953ec6da2454d
Aakashdeveloper <ahanda205@gmail.com>
2020-11-04 20:43:55 +0530

    a

---
+++ 33currencyConverter/cc.js
@@ -0,0 +1,18 @@
+var url = "https://api.exchangeratesapi.io/latest?base="
+
+
+//es6
+function convert(){
+    var base = document.getElementById('base').value;
+    var out = document.getElementById('out').value;
+    var amount = document.getElementById('amount').value;
+    var final;
+    console.log(`${url}${base}`)
+    fetch(`${url}${base}`,{method:'GET'})
+    .then((response) => response.json())
+    .then((data) => {
+        var final = data.rates[out]
+        var result = `Converted value of ${amount} ${base} is ${final} ${out}`
+        document.getElementById('output').innerText = result
+    })
+}
